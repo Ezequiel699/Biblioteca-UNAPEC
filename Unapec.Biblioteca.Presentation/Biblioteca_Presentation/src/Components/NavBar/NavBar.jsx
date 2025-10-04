4a3463259275a74fd00be28ab2362850eaa7cf87
Darwinsalcie <d.salcie1@unapec.edu.do>
2025-10-04 01:05:27 -0400

    Header + estilos listos

---
+++ Unapec.Biblioteca.Presentation/Biblioteca_Presentation/src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,21 @@
+import './NavBar.css'
+
+
+
+const NavBar = () => {
+    return(
+        <header className='header'>
+            <img className='profile-picture' src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="Profile Picture" />
+            <nav className='navbar'>
+                <ul className='navbar-list'>
+                    <li className='navbar-option'><a href="">🔍</a></li>
+                    <li className='navbar-option'><a href="">Home</a></li>
+                    <li className='navbar-option'><a href="">Fast Reading</a></li>
+                    <li className='navbar-option'><a href="">Inmersive</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
+}
+
+export default NavBar;
