260bbec6bfeb7e2d56c75c53ca084779dc1713cf
Darwinsalcie <d.salcie1@unapec.edu.do>
2025-12-17 00:06:25 -0400

    Refactor login component and enhance navigation bar
    Removed `Login.css` and `Login.jsx` as they are no longer needed.
    Updated `NavBar.css` with new styles for improved layout and
    added logout functionality in `NavBar.jsx`, replacing anchor
    tags with `Link` components for better routing.

--- Unapec.Biblioteca.Presentation/Biblioteca_Presentation/src/Components/NavBar/NavBar.jsx
+++ Unapec.Biblioteca.Presentation/Biblioteca_Presentation/src/Components/NavBar/NavBar.jsx
@@ -1,20 +1,33 @@
 import './NavBar.css'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
+import authService from '../Login/authService'
 
 const NavBar = () => {
+    const navigate = useNavigate();
+
+    const handleLogout = () => {
+        if (window.confirm('¿Está seguro que desea cerrar sesión?')) {
+            authService.logout();
+            navigate('/login');
+        }
+    };
+
     return(
         <header className='header'>
             <div className='navbar-container'>
                 <img className='profile-picture' src="https://cdn-icons-png.flaticon.com/512/3135/3135715.png" alt="Profile Picture" />
                 <nav className='navbar'>
                     <ul className='navbar-list'>
-                        <li className='navbar-option'><Link to="/">Home</Link></li>
-                        <li className='navbar-option'><a href="">Books</a></li>
-                        <li className='navbar-option'><a href="">Users</a></li>
-                        <li className='navbar-option'><a href="">Employee</a></li>
+                        <li className='navbar-option'><Link to="/home">Home</Link></li>
+                        <li className='navbar-option'><Link to="/libros">Books</Link></li>
+                        <li className='navbar-option'><Link to="/usuarios">Users</Link></li>
+                        <li className='navbar-option'><Link to="/empleados">Employee</Link></li>
                         <li className='navbar-option'><a href="">🔍</a></li>
                     </ul>
                 </nav>
+                <button className='btn-logout' onClick={handleLogout}>
+                    Logout
+                </button>
             </div>
             <div className='bar'></div>
         </header>
